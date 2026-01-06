0bd8db57093a0491288b8c6288cf0884908f2a7a
gouthamgirish01-ui <gouthamgirish01@gmail.com>
2026-01-06 07:30:01 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,12 @@
+num1= int(input("enter the frist number : "))
+num2= int(input("enter your second number : "))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print()
+print()
+print("Addition : ",add)
+print("Subtraction : ",sub)
+print("multiplication : ",mul)
+print("division : ",div)
